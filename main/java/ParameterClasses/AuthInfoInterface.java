6bdaec1a4f7683d42f34cc35c4b14e59ceb60341
Lance <lance.haderlie@gmail.com>
2019-11-05 18:55:49 -0700

    Beginning stubbing out interfaces

---
+++ main/java/ParameterClasses/AuthInfoInterface.java
@@ -0,0 +1,8 @@
+package java.ParameterClasses;
+
+public abstract class AuthInfoInterface
+{
+    private String userName;
+    private String passWord;
+    private String apiKey;
+}
---
+++ main/java/ParameterClasses/SchemaInterface.java
@@ -0,0 +1,8 @@
+package java.ParameterClasses;
+
+import java.util.Map;
+
+public abstract class SchemaInterface
+{
+    private Map<String, Class> headers;
+}
