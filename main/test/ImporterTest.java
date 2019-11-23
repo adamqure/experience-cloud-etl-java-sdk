8213b0f29368c80458b64a033e1d0dfe53210513
Lance Haderlie <lance.haderlie@gmail.com>
2019-11-22 18:46:41 -0700

    beginning building tests

--- main/test/ImporterTest.java
+++ main/test/ImporterTest.java
@@ -1,5 +1,24 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import java.Importer;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImporterTest {
 
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void testImporterInstantiation()
+    {
+        Importer test = new Importer();
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
 }
