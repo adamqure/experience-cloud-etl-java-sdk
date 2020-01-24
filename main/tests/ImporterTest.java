9b0478f2def7886683431845b8f6cb12878b2a8e
Nick Cummings <nickcummings21@gmail.com>
2020-01-24 12:47:16 -0700

    created file for importer unit tests

---
+++ main/tests/ImporterTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.*;
+
+class ImporterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void upload() {
+    }
+
+    @Test
+    void createJwt() {
+        Importer importer = new Importer();
+        importer.createJwt();
+        System.out.println(importer.getAuthInfo());
+
+    }
+
+    @Test
+    void exchangeJwtAuth() {
+    }
+}
