f37ccf4a18dd1114d8cada2bf9d2c15b69e34d5f
Lance Haderlie <lance.haderlie@gmail.com>
2020-02-03 12:53:06 -0700

    Continuing work on tests

--- main/tests/Tools/ImporterTest.java
+++ main/tests/Tools/ImporterTest.java
@@ -4,11 +4,22 @@ import org.junit.jupiter.api.*;
 
 import java.io.FileNotFoundException;
 import java.security.InvalidKeyException;
-import java.security.spec.InvalidKeySpecException;
 
 class ImporterTest {
     Importer testImp;
-
+    String DataSetID = "5e29e7e984479018a93e70a7";
+    String ExpiredToken = "eyJ4NXUiOiJpbXNfbmExLWtleS0xLmNlciIsImFsZyI6IlJTMjU2In0.eyJpZCI6IjE1ODA3NTU5MzM0OTFfNTM3ZDZi" +
+            "YWQtNmJjOS00YWFjLWEwZTktMDEzM2YyOTFiNzYxX3VlMSIsImNsaWVudF9pZCI6ImQ4YjY1Y2E3NWVlNDRiOGNhOWJmODdiNmZkYzBhMT" +
+            "c0IiwidXNlcl9pZCI6IkQ5Q0I3OEEyNURBRTE0QkMwQTQ5NUMyMUB0ZWNoYWNjdC5hZG9iZS5jb20iLCJzdGF0ZSI6IntcInNlc3Npb25cI" +
+            "jpcImh0dHBzOi8vaW1zLW5hMS5hZG9iZWxvZ2luLmNvbS9pbXMvc2Vzc2lvbi92MS9aakExT1daak1Ea3RabU5tWmkwME1qTTFMV0V5WkRR" +
+            "dFlqUmlNREV3TnpOalpXTTBMUzFFT1VOQ056aEJNalZFUVVVeE5FSkRNRUUwT1RWRE1qRkFkR1ZqYUdGalkzUXVZV1J2WW1VdVkyOXRcIn0" +
+            "iLCJ0eXBlIjoiYWNjZXNzX3Rva2VuIiwiYXMiOiJpbXMtbmExIiwiZmciOiJVRkFLWUpFMkhQRTVKUFVQRzZBTFlQUUFEVT09PT09PSIsIm" +
+            "1vaSI6IjU0YmVlZjg3IiwiYyI6IlRrZnhXK3dwRStzZk9TbGc1RlZITlE9PSIsImV4cGlyZXNfaW4iOiI4NjQwMDAwMCIsInNjb3BlIjoib" +
+            "3BlbmlkLHNlc3Npb24sQWRvYmVJRCxyZWFkX29yZ2FuaXphdGlvbnMsYWRkaXRpb25hbF9pbmZvLnByb2plY3RlZFByb2R1Y3RDb250ZXh0" +
+            "IiwiY3JlYXRlZF9hdCI6IjE1ODA3NTU5MzM0OTEifQ.psJCN3iFkzMx9bgkQBB4cDBHzvuHK6eLT146iw1z-89kf0m0iPqshJuX3ddToUWp" +
+            "3hXEbZWkr9Ta1BezbjTvSnpgtYbNFAs4M2mYnVHpzqCgJQxI41JzQKHAqj94_dHNJIWvHJERnME1L9dX0DHSmFSTSZVwOUZWT7HFdZg-2wP" +
+            "TG4wY3VRVmiwVmmW3lQAJ5aL6N7O1rWUqEEb9tXHM9UJSKeFTdlsmyAX_MV9TK9-zB5kDpkhMK41rQiwUVWzCkB1gawJPutweGv5GiUieOO" +
+            "lwLz0GfD5oH5aoA8FYXt9_hFziQPP55yVoxbYWuOPFMiqRBWmL_zbne8D4Kn7Uwg86399989";
     @BeforeEach
     void setUp() {
         testImp = new Importer();
@@ -23,7 +34,7 @@ class ImporterTest {
     {
 
     }
-
+/** SECTION: CREATEJWT TESTS */
     //This test checks that with a proper config, a valid JWT and Access token are made
     @Test
     void createJwtAndExchange()
@@ -119,6 +130,7 @@ class ImporterTest {
         });
     }
 
+/** SECTION: EXCHANGEJWT TESTS */
     //Attempt to exchange a jwt that is null
     //Should print an error message and throw an exception
     @Test
@@ -130,9 +142,11 @@ class ImporterTest {
         });
     }
 
+    //TODO Async in order to wait for API call
     @Test
     void exchangeExpiredJwt()
     {
+        testImp.getAuthInfo().setJwt(ExpiredToken);
         Assertions.assertThrows(Exception.class, ()->{
            testImp.exchangeJwtAuth();
         });
@@ -162,7 +176,7 @@ class ImporterTest {
         });
     }
 
-
+/** SECTION: UPLOADFILE TESTS */
     //Attempt to upload a file with an empty string for filename
     //An error should be thrown when the file is not found with a
     //corresponding error message
@@ -172,7 +186,7 @@ class ImporterTest {
         testImp.createJwt();
         testImp.exchangeJwtAuth();
         Assertions.assertThrows(FileNotFoundException.class, ()->{
-            testImp.uploadFile("", null, "5e29e7e984479018a93e70a7");
+            testImp.uploadFile("", null, DataSetID);
         });
     }
 
@@ -185,7 +199,32 @@ class ImporterTest {
         testImp.createJwt();
         testImp.exchangeJwtAuth();
         Assertions.assertThrows(FileNotFoundException.class, ()->{
-            testImp.uploadFile(null, null, "5e29e7e984479018a93e70a7");
+            testImp.uploadFile(null, null, DataSetID);
+        });
+    }
+
+    //Attempt to upload a file with an empty string for DataSetID
+    //An error should be thrown with a corresponding error message
+    @Test
+    void uploadEmptyDSId()
+    {
+        testImp.createJwt();
+        testImp.exchangeJwtAuth();
+        Assertions.assertThrows(FileNotFoundException.class, ()->{
+            testImp.uploadFile("Tools/test128.json", null, "");
+        });
+    }
+
+    //Attempt to upload a file with a null for filename
+    //An error should be thrown when attempt to call File fails
+    //with corresponding error message
+    @Test
+    void uploadNullDataSetID()
+    {
+        testImp.createJwt();
+        testImp.exchangeJwtAuth();
+        Assertions.assertThrows(FileNotFoundException.class, ()->{
+            testImp.uploadFile("Tools/test128.json", null, null);
         });
     }
 }
--- main/tests/Tools/IngestorTest.java
+++ main/tests/Tools/IngestorTest.java
@@ -7,11 +7,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class IngestorTest {
     Ingestor testIng;
     AuthInfo testAuth;
+    String DataSetID = "5e29e7e984479018a93e70a7";
+    String ExpiredToken = "eyJ4NXUiOiJpbXNfbmExLWtleS0xLmNlciIsImFsZyI6IlJTMjU2In0.eyJpZCI6IjE1ODA3NTU5MzM0OTFfNTM3ZDZi" +
+            "YWQtNmJjOS00YWFjLWEwZTktMDEzM2YyOTFiNzYxX3VlMSIsImNsaWVudF9pZCI6ImQ4YjY1Y2E3NWVlNDRiOGNhOWJmODdiNmZkYzBhMT" +
+            "c0IiwidXNlcl9pZCI6IkQ5Q0I3OEEyNURBRTE0QkMwQTQ5NUMyMUB0ZWNoYWNjdC5hZG9iZS5jb20iLCJzdGF0ZSI6IntcInNlc3Npb25cI" +
+            "jpcImh0dHBzOi8vaW1zLW5hMS5hZG9iZWxvZ2luLmNvbS9pbXMvc2Vzc2lvbi92MS9aakExT1daak1Ea3RabU5tWmkwME1qTTFMV0V5WkRR" +
+            "dFlqUmlNREV3TnpOalpXTTBMUzFFT1VOQ056aEJNalZFUVVVeE5FSkRNRUUwT1RWRE1qRkFkR1ZqYUdGalkzUXVZV1J2WW1VdVkyOXRcIn0" +
+            "iLCJ0eXBlIjoiYWNjZXNzX3Rva2VuIiwiYXMiOiJpbXMtbmExIiwiZmciOiJVRkFLWUpFMkhQRTVKUFVQRzZBTFlQUUFEVT09PT09PSIsIm" +
+            "1vaSI6IjU0YmVlZjg3IiwiYyI6IlRrZnhXK3dwRStzZk9TbGc1RlZITlE9PSIsImV4cGlyZXNfaW4iOiI4NjQwMDAwMCIsInNjb3BlIjoib" +
+            "3BlbmlkLHNlc3Npb24sQWRvYmVJRCxyZWFkX29yZ2FuaXphdGlvbnMsYWRkaXRpb25hbF9pbmZvLnByb2plY3RlZFByb2R1Y3RDb250ZXh0" +
+            "IiwiY3JlYXRlZF9hdCI6IjE1ODA3NTU5MzM0OTEifQ.psJCN3iFkzMx9bgkQBB4cDBHzvuHK6eLT146iw1z-89kf0m0iPqshJuX3ddToUWp" +
+            "3hXEbZWkr9Ta1BezbjTvSnpgtYbNFAs4M2mYnVHpzqCgJQxI41JzQKHAqj94_dHNJIWvHJERnME1L9dX0DHSmFSTSZVwOUZWT7HFdZg-2wP" +
+            "TG4wY3VRVmiwVmmW3lQAJ5aL6N7O1rWUqEEb9tXHM9UJSKeFTdlsmyAX_MV9TK9-zB5kDpkhMK41rQiwUVWzCkB1gawJPutweGv5GiUieOO" +
+            "lwLz0GfD5oH5aoA8FYXt9_hFziQPP55yVoxbYWuOPFMiqRBWmL_zbne8D4Kn7Uwg86399989";
     @BeforeEach
     void setUp() {
         testIng = new Ingestor();
@@ -24,7 +35,7 @@ class IngestorTest {
     @AfterEach
     void tearDown() {
     }
-
+/** SECTION, CREATE BATCH TESTS */
     //Attempt to create a batch without a dataset ID
     @Test
     void createBatchEmptyDSID()
@@ -43,42 +54,13 @@ class IngestorTest {
         });
     }
 
-    //Attempt to create a batch with a null authInfo Object
-    @Test
-    void createBatchNullAuthInfo()
-    {
-        Assertions.assertThrows(Exception.class, ()->{
-           testIng.createBatch(null, "5e29e7e984479018a93e70a7");
-        });
-    }
-
-    //Attempt to create a batch with an empty string in Access Token
-    @Test
-    void createBatchEmptyAuthToken()
-    {
-        testAuth.setAccessToken(new AuthToken());
-        Assertions.assertThrows(Exception.class, ()->{
-            testIng.createBatch(testAuth, "5e29e7e984479018a93e70a7");
-        });
-    }
-
-    //Test creating a batch with an empty string in IMS Org
-    @Test
-    void createBatchEmptyImsOrg()
-    {
-        testAuth.setImsOrgId("");
-        Assertions.assertThrows(Exception.class, ()->{
-            testIng.createBatch(testAuth, "5e29e7e984479018a93e70a7");
-        });
-    }
-
     //Test creating a batch with a null in API key
     @Test
     void createBatchNullAPI()
     {
         testAuth.setApiKey(null);
         Assertions.assertThrows(Exception.class, ()->{
-            testIng.createBatch(testAuth, "5e29e7e984479018a93e70a7");
+            testIng.createBatch(testAuth, DataSetID);
         });
     }
 
@@ -88,7 +70,17 @@ class IngestorTest {
     {
         testAuth.setApiKey("");
         Assertions.assertThrows(Exception.class, ()->{
-            testIng.createBatch(testAuth, "5e29e7e984479018a93e70a7");
+            testIng.createBatch(testAuth, DataSetID);
+        });
+    }
+
+    //Test creating a batch with an empty string in IMS Org
+    @Test
+    void createBatchEmptyImsOrg()
+    {
+        testAuth.setImsOrgId("");
+        Assertions.assertThrows(Exception.class, ()->{
+            testIng.createBatch(testAuth, DataSetID);
         });
     }
 
@@ -98,7 +90,7 @@ class IngestorTest {
     {
         testAuth.setImsOrgId(null);
         Assertions.assertThrows(Exception.class, ()->{
-            testIng.createBatch(testAuth, "5e29e7e984479018a93e70a7");
+            testIng.createBatch(testAuth, DataSetID);
         });
     }
 
@@ -109,17 +101,37 @@ class IngestorTest {
     {
         Assertions.assertThrows(Exception.class, ()-> {
             testIng.createBatch(new AuthInfo("", "", ""),
-                    "5e29e7e984479018a93e70a7");
+                    DataSetID);
         });
     }
 
+    //Attempt to create a batch with a null authInfo Object
+    @Test
+    void createBatchNullAuthInfo()
+    {
+        Assertions.assertThrows(Exception.class, ()->{
+            testIng.createBatch(null, DataSetID);
+        });
+    }
+
+    //Attempt to create a batch with an empty string in Access Token
+    @Test
+    void createBatchEmptyAuthToken()
+    {
+        testAuth.setAccessToken(new AuthToken());
+        Assertions.assertThrows(Exception.class, ()->{
+            testIng.createBatch(testAuth, DataSetID);
+        });
+    }
+
+/** SECTION, UPLOAD FILE TESTS */
     //Attempt to upload to an already created batch with an empty string for filename
     @Test
     void uploadFileEmptyFilename()
     {
-        String batchID = testIng.createBatch(testAuth, "5e29e7e984479018a93e70a7");
+        String batchID = testIng.createBatch(testAuth, DataSetID);
         Assertions.assertThrows(Exception.class, ()->{
-            testIng.uploadFileToBatch(testAuth, null, batchID, "5e29e7e984479018a93e70a7", "");
+            testIng.uploadFileToBatch(testAuth, null, batchID, DataSetID, "");
         });
     }
 
@@ -127,15 +139,67 @@ class IngestorTest {
     @Test
     void uploadFileNullFilename()
     {
-        String batchID = testIng.createBatch(testAuth, "5e29e7e984479018a93e70a7");
+        String batchID = testIng.createBatch(testAuth, DataSetID);
         Assertions.assertThrows(Exception.class, ()->{
-            testIng.uploadFileToBatch(testAuth, null, batchID, "5e29e7e984479018a93e70a7", null);
+            testIng.uploadFileToBatch(testAuth, null, batchID, DataSetID, null);
+        });
+    }
+
+    //Attempt to upload to an already created batch with an empty string in batchID
+    @Test
+    void uploadFileEmptyBatchID()
+    {
+        String batchID = "";
+        Assertions.assertThrows(Exception.class, ()->{
+            testIng.uploadFileToBatch(testAuth, null, batchID, DataSetID, null);
+        });
+    }
+
+    //Attempt to upload to an already created batch with a null in batchID
+    @Test
+    void uploadFileNullBatchID()
+    {
+        String batchID = null;
+        Assertions.assertThrows(Exception.class, ()->{
+            testIng.uploadFileToBatch(testAuth, null, batchID, DataSetID, null);
+        });
+    }
+
+/** SECTION, CANCEL BATCH TESTS */
+    @Test
+    void cancelBatchEmptyAuthInfo()
+    {
+        String batchID = testIng.createBatch(testAuth, DataSetID);
+        testAuth = new AuthInfo("", "", "");
+        Assertions.assertThrows(Exception.class, ()->{
+            testIng.cancelBatch(testAuth, batchID);
         });
     }
 
     @Test
-    void generateHeadersEmptyAuthInfo()
+    void cancelBatchNullAuthInfo()
     {
-
+        String batchID = testIng.createBatch(testAuth, DataSetID);
+        testAuth = null;
+        Assertions.assertThrows(Exception.class, ()->{
+            testIng.cancelBatch(testAuth, batchID);
+        });
     }
+
+    @Test
+    void cancelBatchEmptyBatchID()
+    {
+        Assertions.assertThrows(Exception.class, ()->{
+            testIng.cancelBatch(testAuth, "");
+        });
+    }
+
+    @Test
+    void cancelBatchNullBatchID()
+    {
+        Assertions.assertThrows(Exception.class, ()->{
+            testIng.cancelBatch(testAuth, null);
+        });
+    }
+
 }
